160b07b65bd3a4d6da4f117883065632c47fa920
beaukams <kamstelecom@hotmail.com>
2016-11-06 21:05:00 +0000

    ajout bundle Admin, User et Forum

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,9 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new ApplicationBundle\ApplicationBundle(),
+            new AdminBundle\AdminBundle(),
+            new UserBundle\UserBundle(),
+            new ForumBundle\ForumBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
